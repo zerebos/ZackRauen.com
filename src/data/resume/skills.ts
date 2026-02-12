5485d3b6705842ae14f2b936ee6fb8672ff484f8
Zerebos <me@zerebos.com>
2026-02-12 05:28:19 -0500

    Update some information and a project

--- src/data/resume/skills.ts
+++ src/data/resume/skills.ts
@@ -9,11 +9,11 @@ export default {
             ]
         },
         {
-            label: "JavaScript",
+            label: "TypeScript",
             value: 90,
             subskills: [
-                {label: "Node.js", value: 90},
-                {label: "Web", value: 75}
+                {label: "Backend", value: 90},
+                {label: "Frontend", value: 75}
             ]
         },
         {
@@ -29,11 +29,11 @@ export default {
         {label: "Hardware Design", value: 80},
         {label: "Java", value: 65},
         {label: "C#", value: 65},
-        {label: "PHP", value: 55},
+        {label: "PHP", value: 65},
         {label: "VHDL", value: 60},
-        {label: "SQL", value: 50},
+        {label: "SQL", value: 60},
         {label: "C", value: 90},
-        {label: "Go", value: 40},
+        {label: "Go", value: 70},
         {label: "Python", value: 80},
         {label: "Assembly", value: 85},
         {label: "R", value: 45},
